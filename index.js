4036be06e9cdf4361e767c52ec3b38708499cfec
Z3K3GamingYT <83733589+Z3K3GamingYT@users.noreply.github.com>
2021-11-11 08:37:48 -0800

    index.js v1.1.5.3
    Fixed minor bugs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var os = require("os");
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000)); // DO NOT CHANGE
 
 app.get('/', function (request, response) {
     var result = 'App is running'

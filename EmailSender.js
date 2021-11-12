d0d4365e81b51f65afba569216ec1c631da95688
Z3K3GamingYT <83733589+Z3K3GamingYT@users.noreply.github.com>
2021-11-12 13:35:43 -0800

    EmailSender.js v1

---
+++ EmailSender.js
@@ -0,0 +1,10 @@
+Email.send({
+    SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+    To : '@email.com',
+    From : "AltifyHosting@isp.com",
+    Subject : "Thanks",
+    Body : "Thanks for using the Altify bot service"
+}).then(
+  message => alert(message)
+);
+<script src="https://smtpjs.com/v3/smtp.js"></script>
